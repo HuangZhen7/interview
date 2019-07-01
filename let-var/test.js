b92dabd5315cb8c27665700c13558b5a4372aca3
HuangZhen7 <HuangZhen_49@163.com>
2019-07-01 16:34:26 +0800

    let和var

---
+++ let-var/test.js
@@ -0,0 +1,8 @@
+var a = 99;            // 全局变量a
+f();                   // f是函数，虽然定义在调用的后面，但是函数声明会提升到作用域的顶部。 
+console.log(a);        // a=>99,  此时是全局变量的a
+function f() {
+  console.log(a);      // 当前的a变量是下面变量a声明提升后，默认值undefined
+  var a = 10;
+  console.log(a);      // a => 10
+}
---
+++ let-var/test2.js
@@ -0,0 +1,9 @@
+// { 
+//   var i = 9;
+// } 
+// console.log(i);  // 9
+
+{ 
+  let i = 9;     // i变量只在 花括号内有效！！！
+} 
+console.log(i);  // Uncaught ReferenceError: i is not defined
